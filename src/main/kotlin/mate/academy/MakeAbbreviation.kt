c81225f1f333cc97406aec61a523863f019b8373
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-10-26 20:05:01 +0100

    Task completed

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,19 @@
 package mate.academy
+//Багато речей у світі складаються з кількох слів.
+// Ми хочемо створити makeAbbr()метод,
+// який повертає їхні скорочення.
 
 fun makeAbbr(source: String) : String {
     // implement this function
-    return ""
+    return source
+        .split(' ')
+        .filter { it.isNotEmpty()}
+        .map {it.first().toString()}
+        .joinToString ("")
+        .uppercase()
+}
+fun main() {
+    println(makeAbbr("nayional aeronautics space adminisration"))
+    println(makeAbbr("central processing unit"))
+    println(makeAbbr("simlified molecular input line entry specification"))
 }
